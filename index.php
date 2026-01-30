ad5573c0c1395b5e3559876b1a958df89e3e1670
DerWaldFried <verwaltung@fabelgrund.de>
2026-01-30 01:03:23 +0100

    changed required imports. Added Asset and resorted

--- index.php
+++ index.php
@@ -5,9 +5,10 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+require_once __DIR__ . '/app/Core/Asset.php';
+require_once __DIR__ . '/app/Core/Controller.php';
 require_once __DIR__ . '/app/Core/Template.php';
 require_once __DIR__ . '/app/Core/View.php';
-require_once __DIR__ . '/app/Core/Controller.php';
 require_once __DIR__ . '/app/Controller/HomeController.php';
 
 $controller = new HomeController();
