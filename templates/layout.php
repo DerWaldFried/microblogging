19453de79ddf31867871915ecea7dbba7c1751a5
DerWaldFried <verwaltung@fabelgrund.de>
2026-01-30 01:29:04 +0100

    Changed title php include to <?= too for simple output title

--- templates/layout.php
+++ templates/layout.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title><?php htmlspecialchars($title) ?></title>
+    <title><?= htmlspecialchars($title) ?></title>
 
     <?= Asset::renderCss() ?>
 </head>
