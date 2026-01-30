eb65e1d8cf820afbc394620ca2d2ce9f03bd0909
DerWaldFried <verwaltung@fabelgrund.de>
2026-01-30 01:48:41 +0100

    Set Comments for more Code Informations

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -4,9 +4,11 @@ class HomeController extends Controller
 {
     public function index(): string
     {
+        // Add necessary CSS assets
         Asset::addCss('/assets/css/main.css');
         Asset::addCss('/assets/css/home.css');
 
+        // Render the home view with data
         return $this->view('home', [
             'title' => 'FabelMicroblog - Startseite',
             'message' => 'Welcome to FabelMicroblog!'
