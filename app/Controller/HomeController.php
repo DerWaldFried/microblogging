ab4f2731d47c5603767f6457a2db5edf57d4775a
DerWaldFried <verwaltung@fabelgrund.de>
2026-01-30 01:04:03 +0100

    Adding Asset Class und Call to Function addCss

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -2,10 +2,14 @@
 
 class HomeController extends Controller
 {
-    public function index(): string {
+    public function index(): string
+    {
+        Asset::addCss('/assets/css/main.css');
+        Asset::addCss('/assets/css/home.css');
+
         return $this->view('home', [
-                'title' => 'FabelMicroblog - Startseite',
-                'message' => 'Welcome to FabelMicroblog!'
-            ]);
+            'title' => 'FabelMicroblog - Startseite',
+            'message' => 'Welcome to FabelMicroblog!'
+        ]);
     }
 }
